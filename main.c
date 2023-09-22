56c175180f3a78ce77e9de897bacb834ee4b6a14
somyeong04 <144991811+somyeong04@users.noreply.github.com>
2023-09-22 15:15:39 +0900

    2

--- main.c
+++ main.c
@@ -3,17 +3,13 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void){
-   int input_int;
-   float input_float;
-   
-   printf("enter an integer : ");
-   scanf("%d", &input_int);
-   
-   printf("enter a float : ");
-   scanf("%f", &input_float);
-   
-   printf("integer %d, float : %f\n", input_int, input_float);
-   
-   return 0;
-} 
+int main(void) {
+	char c;
+	int i;
+	printf("input a number");
+	scanf("%c", &c);
+	i = c-'0';
+	printf("The input number ASCII is %i\n", c);
+	printf("The input number is %i\n", i);
+	return 0;
+}
