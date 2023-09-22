7203ff8334ce183704af59a86aa03c91f84ded9e
somyeong04 <144991811+somyeong04@users.noreply.github.com>
2023-09-22 15:17:30 +0900

    3

--- main.c
+++ main.c
@@ -3,13 +3,16 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void) {
-	char c;
-	int i;
-	printf("input a number");
-	scanf("%c", &c);
-	i = c-'0';
-	printf("The input number ASCII is %i\n", c);
-	printf("The input number is %i\n", i);
-	return 0;
+int main(void){
+   char c;
+   int i;
+   
+   printf("enter a character:");
+   scanf("%c", &c);
+   
+   i=c;
+   
+   printf("The next character of %c(%d) is %c(%d)", c, i, c+1, i+1);
+   
+   return 0;
 }
